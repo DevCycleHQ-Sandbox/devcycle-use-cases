51898fb117464a01a50a389f14636a1b88ec5f5a
Mark C Allen <mark@markcallen.com>
2023-08-15 23:27:31 -0700

    Added confetti is the user is optin

--- src/app/2-user-optin/page.tsx
+++ src/app/2-user-optin/page.tsx
@@ -14,6 +14,7 @@ import {
 import { SyncLoader } from 'react-spinners';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useVariableValue, useDVCClient } from '@devcycle/devcycle-react-sdk';
+import Confetti from 'react-confetti';
 
 import { AuthRequiredError } from '../../lib/exceptions';
 
@@ -22,10 +23,23 @@ const RootUserOptIn = () => {
   const [optIn, setOptIn] = useState<boolean>(false);
   const dvcClient = useDVCClient();
 
+  const userOptIn = useVariableValue('user-optin', false);
+
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0
+  });
+  useEffect(() => {
+    const { innerWidth: width, innerHeight: height } = window;
+    setDimensions({
+      width,
+      height
+    });
+  }, [userOptIn]);
+
   useEffect(() => {
     if (user?.email) {
-      const userObj = { email: user.email, customData: { optIn } };
-      console.log({ userObj });
+      const userObj = { email: user.email, customData: { optin: optIn } };
       dvcClient
         .identifyUser(userObj)
         .then((variables) => console.log('Updated Variables:', variables));
@@ -51,6 +65,9 @@ const RootUserOptIn = () => {
   return (
     <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
       <Flex p={8} flex={1} justify={'center'}>
+        {userOptIn && (
+          <Confetti width={dimensions.width} height={dimensions.height} />
+        )}
         <Stack spacing={6} w={'full'} maxW={'lg'}>
           <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
             <Text color={'#1F2937'} as={'span'}>
